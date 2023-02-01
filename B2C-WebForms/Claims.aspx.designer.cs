bdc1ae324de2488ee1d7f58fd10b716654541f9f
Fsanzana <f.sanzana01@ufromail.cl>
2023-02-01 17:36:17 -0300

    Project upload

---
+++ B2C-WebForms/Claims.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace B2C_WebForms
+{
+
+
+    public partial class Claims
+    {
+    }
+}
